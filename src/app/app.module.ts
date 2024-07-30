638a674bba149d22fa8c3204c5dea1d79596a6d9
Franco <fraffa1990@gmail.com>
2024-07-30 19:34:30 -0300

    se modifico el idioma global de la app (LOCALE_ID) y se usó el Fieldset para redereizar la info en el layout del page uncommon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
@@ -7,6 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './sahred/shared.module';
 import { ProductsModule } from './products/products.module';
+
+//Configuración del locale de la app
+import localeEsAR from '@angular/common/locales/es-AR'
+
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData ( localeEsAR );
 
 
 
@@ -24,7 +31,11 @@ import { ProductsModule } from './products/products.module';
     
     
 ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'es-AR' //para establecer un idioma por defecto en la app
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
