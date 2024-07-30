820dcec390a5f20fb876c41ed6b790d506db1601
Franco <fraffa1990@gmail.com>
2024-07-30 02:04:28 -0300

    se creó la UI basic con PrimeNG: un menuBar con lazyloading y routerLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './sahred/shared.module';
+import { ProductsModule } from './products/products.module';
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,8 +17,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    SharedModule,
+    ProductsModule
+    
+    
+],
   providers: [],
   bootstrap: [AppComponent]
 })
